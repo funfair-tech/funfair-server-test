46ad0fe046c8d73cd314f4f8d659fec5e03490c5
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-11-22 15:53:18 +0000

    Added ValidatorTestBase for testing validators

--- src/FunFair.Test.Common/TestBase.cs
+++ src/FunFair.Test.Common/TestBase.cs
@@ -41,3 +41,4 @@ namespace FunFair.Test.Common
         }
     }
 }
+
---
+++ src/FunFair.Test.Common/ValidatorTestsBase.cs
@@ -0,0 +1,97 @@
+using System.Linq;
+using FluentValidation;
+using FluentValidation.Results;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FunFair.Test.Common
+{
+    /// <summary>
+    ///     Base class for testing validators.
+    /// </summary>
+    /// <typeparam name="TValidator">The validator to test.</typeparam>
+    /// <typeparam name="TObject">The object the validator tests</typeparam>
+    public abstract class ValidatorTestsBase<TValidator, TObject> : LoggingTestBase
+        where TValidator : AbstractValidator<TObject>, new()
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="output">Test output.</param>
+        protected ValidatorTestsBase(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        /// <summary>
+        ///     Gets the validator
+        /// </summary>
+        protected TValidator Validator { get; } = new TValidator();
+
+        /// <summary>
+        ///     Outputs the validation results.
+        /// </summary>
+        /// <param name="result">The validation results</param>
+        protected void Dump(ValidationResult result)
+        {
+            if (!result.Errors.Any())
+            {
+                return;
+            }
+
+            this.Output.WriteLine($"Found {result.Errors.Count} errors:");
+
+            foreach (ValidationFailure error in result.Errors.OrderBy(keySelector: e => e.PropertyName)
+                .ThenBy(keySelector: e => e.ErrorMessage))
+            {
+                this.Output.WriteLine($" * {error.PropertyName} : {error.ErrorMessage}");
+            }
+        }
+
+        /// <summary>
+        ///     Creates an instance of an object that is valid.
+        /// </summary>
+        /// <returns>A valid object instance.</returns>
+        protected abstract TObject CreateAValidObject();
+
+        /// <summary>
+        ///     Checks that everything is valid
+        /// </summary>
+        protected abstract void EverythingValid();
+
+        /// <summary>
+        /// Tests that all properties in the object pass validation.
+        /// </summary>
+        protected void TestEverythingValid()
+        {
+            TObject itemToValidate = this.CreateAValidObject();
+
+            ValidationResult result = this.Validator.Validate(itemToValidate);
+
+            this.Dump(result);
+
+            Assert.Equal(expected: 0, result.Errors.Count);
+        }
+
+        /// <summary>
+        /// Check that only the named property has errors.
+        /// </summary>
+        /// <param name="result">The validation result.</param>
+        /// <param name="erroringProperty">The property expected to have errors.</param>
+        protected static void AssertOnlyNamedPropertyHasErrors(ValidationResult result, string erroringProperty)
+        {
+            Assert.True(result.Errors.All(predicate: e => e.PropertyName == erroringProperty),
+                        $"Should only have had errors in {erroringProperty}, but found errors in {string.Join(separator: ",", result.Errors.Select(selector: e => e.PropertyName).Distinct())}");
+        }
+
+        /// <summary>
+        /// Builds the property name from a succession of parts.
+        /// </summary>
+        /// <param name="parts">The parts.</param>
+        /// <returns>The property name.</returns>
+        protected static string MakePropertyName(params string[] parts)
+        {
+            return string.Join(separator: ".", parts);
+        }
+    }
+}
