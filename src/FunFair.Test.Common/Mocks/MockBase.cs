c743e982b7d3e68e1bf8f28018d660f124ef1983
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-12-04 18:12:08 +0000

    Added mock base from FunServer so it can be put in other places

---
+++ src/FunFair.Test.Common/Mocks/MockBase.cs
@@ -0,0 +1,42 @@
+namespace FunFair.Test.Common.Mocks
+{
+    /// <summary>
+    ///     Base class for value.
+    /// </summary>
+    /// <typeparam name="T">The type of the mock.</typeparam>
+    public abstract class MockBase<T>
+    {
+        private readonly T _value;
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="value">The reference instance value.</param>
+        protected MockBase(T value)
+        {
+            this._value = value;
+        }
+
+        /// <summary>
+        ///     Gets the reference object instance.
+        /// </summary>
+        /// <param name="instance">The instance to convert.</param>
+        /// <returns>The instance value.</returns>
+        public static implicit operator T(MockBase<T> instance)
+        {
+            return instance._value;
+        }
+
+        /// <summary>
+        ///     Gets a new T
+        /// </summary>
+        /// <returns></returns>
+        public abstract T Next();
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return this._value.ToString();
+        }
+    }
+}
