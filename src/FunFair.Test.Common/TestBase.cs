ce92df0d8520b10398f91a1694eab451dd8021af
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-02-11 14:30:59 +0000

    Renamed methods

--- src/FunFair.Test.Common/TestBase.cs
+++ src/FunFair.Test.Common/TestBase.cs
@@ -11,12 +11,12 @@ namespace FunFair.Test.Common
     public abstract class TestBase
     {
         /// <summary>
-        /// Constructor.
+        ///     Constructor.
         /// </summary>
         protected TestBase()
         {
             // Nothing to do here!
-            Assert.False(false, "Because");
+            Assert.False(condition: false, userMessage: "Because");
         }
 
         /// <summary>
@@ -25,7 +25,7 @@ namespace FunFair.Test.Common
         /// <param name="value">The value to return.</param>
         /// <typeparam name="T">The type to return.</typeparam>
         /// <returns>An optional result</returns>
-        protected static Task<T?> FromOptionalResult<T>(T? value)
+        protected static Task<T?> FromOptionalResultAsync<T>(T? value)
             where T : class
         {
             return Task.FromResult(value);
@@ -36,14 +36,14 @@ namespace FunFair.Test.Common
         /// </summary>
         /// <typeparam name="T">The type to return.</typeparam>
         /// <returns>A task with a null result.</returns>
-        protected static Task<T?> NullResult<T>()
+        protected static Task<T?> NullResultAsync<T>()
             where T : class
         {
-            return FromOptionalResult((T?) null);
+            return FromOptionalResultAsync((T?) null);
         }
 
         /// <summary>
-        /// Assert that the item is not null and return the non-null value.
+        ///     Assert that the item is not null and return the non-null value.
         /// </summary>
         /// <param name="value">The value</param>
         /// <typeparam name="T">The item type</typeparam>
@@ -63,4 +63,3 @@ namespace FunFair.Test.Common
         }
     }
 }
-
