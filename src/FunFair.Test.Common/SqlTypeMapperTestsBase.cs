3e5794b8d0400e5c9bef3288be15bc2f765a2b75
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-05-05 18:26:28 +0100

    Added Type Mapper Base class

---
+++ src/FunFair.Test.Common/SqlTypeMapperTestsBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Dapper;
+using NSubstitute;
+using Xunit;
+
+namespace FunFair.Test.Common
+{
+    public abstract class SqlTypeMapperTestsBase<TTypeMapper, TMappedType>
+        where TTypeMapper : SqlMapper.TypeHandler<TMappedType>, new()
+    {
+        private readonly TTypeMapper _handler;
+
+        protected SqlTypeMapperTestsBase()
+        {
+            this._handler = new TTypeMapper();
+        }
+
+        protected void Parse(object value, TMappedType expected)
+        {
+            TMappedType result = this._handler.Parse(value);
+
+            Assert.Equal(expected, result);
+        }
+
+        protected void SetValue(TMappedType value, object expected)
+        {
+            IDbDataParameter parameter = Substitute.For<IDbDataParameter>();
+
+            this._handler.SetValue(parameter, value);
+
+            parameter.Received(requiredNumberOfCalls: 1)
+                     .Value = expected;
+        }
+    }
+}
