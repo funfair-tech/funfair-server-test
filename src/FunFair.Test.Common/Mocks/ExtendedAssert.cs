067672f568d807d8f70f62e6f575f386b5b302c6
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-07-03 08:43:58 +0100

    FF-1429 - Fixed up code analysis options Added overrides for ExtendedAssert to take serialiser options

--- src/FunFair.Test.Common/Mocks/ExtendedAssert.cs
+++ src/FunFair.Test.Common/Mocks/ExtendedAssert.cs
@@ -9,6 +9,8 @@ namespace FunFair.Test.Common.Mocks
     /// </summary>
     public static class ExtendedAssert
     {
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions {WriteIndented = true};
+
         /// <summary>
         ///     Assert if two objects are deep equal by.
         /// </summary>
@@ -16,9 +18,7 @@ namespace FunFair.Test.Common.Mocks
         /// <param name="actual">The actual value.</param>
         public static void DeepEqual<T>(T expected, T actual)
         {
-            string expectedString = JsonSerializer.Serialize(expected);
-            string actualString = JsonSerializer.Serialize(actual);
-            Assert.Equal(expected: expectedString, actual: actualString);
+            DeepEqual(expected: expected, actual: actual, jsonSerializerOptions: SerializerOptions);
         }
 
         /// <summary>
@@ -28,8 +28,32 @@ namespace FunFair.Test.Common.Mocks
         /// <param name="actual">The actual value.</param>
         public static void DeepEqual<T>(IReadOnlyList<T> expected, IReadOnlyList<T> actual)
         {
-            string expectedString = JsonSerializer.Serialize(expected);
-            string actualString = JsonSerializer.Serialize(actual);
+            DeepEqual(expected: expected, actual: actual, jsonSerializerOptions: SerializerOptions);
+        }
+
+        /// <summary>
+        ///     Assert if two objects are deep equal by.
+        /// </summary>
+        /// <param name="expected">The expected object value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="jsonSerializerOptions">Serializer Options.</param>
+        public static void DeepEqual<T>(T expected, T actual, JsonSerializerOptions jsonSerializerOptions)
+        {
+            string expectedString = JsonSerializer.Serialize(value: expected, options: jsonSerializerOptions);
+            string actualString = JsonSerializer.Serialize(value: actual, options: jsonSerializerOptions);
+            Assert.Equal(expected: expectedString, actual: actualString);
+        }
+
+        /// <summary>
+        ///     Assert if two objects are deep equal by.
+        /// </summary>
+        /// <param name="expected">The expected object value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="jsonSerializerOptions">Serializer Options.</param>
+        public static void DeepEqual<T>(IReadOnlyList<T> expected, IReadOnlyList<T> actual, JsonSerializerOptions jsonSerializerOptions)
+        {
+            string expectedString = JsonSerializer.Serialize(value: expected, options: jsonSerializerOptions);
+            string actualString = JsonSerializer.Serialize(value: actual, options: jsonSerializerOptions);
             Assert.Equal(expected: expectedString, actual: actualString);
         }
     }
