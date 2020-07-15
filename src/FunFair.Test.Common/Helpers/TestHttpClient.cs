67a014102e41e53af34ad25244dd32a1186578f4
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-07-15 16:48:17 +0100

    Set URL and an overload to pass in serializer options

--- src/FunFair.Test.Common/Helpers/TestHttpClient.cs
+++ src/FunFair.Test.Common/Helpers/TestHttpClient.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
@@ -11,16 +13,18 @@ namespace FunFair.Test.Common.Helpers
     /// </summary>
     public static class TestHttpClient
     {
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions();
+
         /// <summary>
         ///     Creates the client
         /// </summary>
         /// <param name="httpStatusCode">HTTP status code to be returned.</param>
         /// <param name="responseMessage">Response message string.</param>
         /// <returns></returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>")]
+        [SuppressMessage(category: "Reliability", checkId: "CA2000:Dispose objects before losing scope", Justification = "For unit tests caller to dispose")]
         public static HttpClient Create(HttpStatusCode httpStatusCode, string responseMessage)
         {
-            return new HttpClient(new FakeHttpMessageHandler(httpStatusCode, responseMessage));
+            return new HttpClient(new FakeHttpMessageHandler(statusCode: httpStatusCode, responseMessage: responseMessage)) {BaseAddress = new Uri("https://localhost")};
         }
 
         /// <summary>
@@ -30,7 +34,7 @@ namespace FunFair.Test.Common.Helpers
         /// <returns>HTTP status code to be returned.</returns>
         public static HttpClient Create(HttpStatusCode httpStatusCode)
         {
-            return Create(httpStatusCode, string.Empty);
+            return Create(httpStatusCode: httpStatusCode, responseMessage: string.Empty);
         }
 
         /// <summary>
@@ -41,13 +45,25 @@ namespace FunFair.Test.Common.Helpers
         /// <returns>HTTP status code to be returned.</returns>
         public static HttpClient Create<T>(HttpStatusCode httpStatusCode, T responseObject)
         {
-            return Create(httpStatusCode, JsonSerializer.Serialize(responseObject, new JsonSerializerOptions()));
+            return Create(httpStatusCode: httpStatusCode, JsonSerializer.Serialize(value: responseObject, options: SerializerOptions));
+        }
+
+        /// <summary>
+        ///     Creates the client
+        /// </summary>
+        /// <param name="httpStatusCode">HTTP status code to be returned.</param>
+        /// <param name="responseObject">Response object to return.</param>
+        /// <param name="jsonSerializerOptions">The JSON serializer options to use.</param>
+        /// <returns>HTTP status code to be returned.</returns>
+        public static HttpClient Create<T>(HttpStatusCode httpStatusCode, T responseObject, JsonSerializerOptions jsonSerializerOptions)
+        {
+            return Create(httpStatusCode: httpStatusCode, JsonSerializer.Serialize(value: responseObject, options: jsonSerializerOptions));
         }
 
         private sealed class FakeHttpMessageHandler : HttpMessageHandler
         {
-            private readonly HttpStatusCode _statusCode;
             private readonly string _responseMessage;
+            private readonly HttpStatusCode _statusCode;
 
             public FakeHttpMessageHandler(HttpStatusCode statusCode, string responseMessage)
             {
@@ -57,7 +73,7 @@ namespace FunFair.Test.Common.Helpers
 
             protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
             {
-                HttpResponseMessage httpResponseMessage = new HttpResponseMessage(this._statusCode) { Content = new StringContent(this._responseMessage) };
+                HttpResponseMessage httpResponseMessage = new HttpResponseMessage(this._statusCode) {Content = new StringContent(this._responseMessage)};
 
                 return Task.FromResult(httpResponseMessage);
             }
