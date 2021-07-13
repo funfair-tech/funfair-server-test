193cff47d0c71e31deeaaf8569bbc4ff424ed0de
Vladimir Nesic <vladimir.nesic@funfair.eu>
2021-07-13 14:09:50 +0200

    use NonBlocking concurrent dictionary

--- src/FunFair.Test.Common/Mocks/MockLogger.cs
+++ src/FunFair.Test.Common/Mocks/MockLogger.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Concurrent;
+using NonBlocking;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Logging;
