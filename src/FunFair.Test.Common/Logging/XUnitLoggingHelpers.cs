5c861ce838841b941744124d3064f1ce48c98760
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-01-17 00:15:07 +0000

    Fixed code analysis issues

--- src/FunFair.Test.Common/Logging/XUnitLoggingHelpers.cs
+++ src/FunFair.Test.Common/Logging/XUnitLoggingHelpers.cs
@@ -8,7 +8,7 @@ internal static class XUnitLoggingHelpers
 {
     [SuppressMessage(category: "Microsoft.Reliability", checkId: "CA2000:DisposeObjectsBeforeLosingScope", Justification = "A mock of unit tests")]
     [SuppressMessage(category: "codecracker.CSharp", checkId: "CC0022:DisposeObjectsBeforeLosingScope", Justification = "A mock of unit tests")]
-    [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Local", Justification = "May be used in derived test classes")]
+    [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global", Justification = "Used as part of test infrastructure")]
     public static ILoggingBuilder AddXUnit(this ILoggingBuilder builder, ITestOutputHelper output)
     {
         return builder.AddProvider(new XunitLoggerProvider(output));
