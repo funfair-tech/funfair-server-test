b8c98daa748757be27c8e5827b25a7069e06f11a
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-10 22:08:14 +0000

    Code generated logging

--- src/FunFair.Test.Common.Tests/Extensions/LogExtensions.cs
+++ src/FunFair.Test.Common.Tests/Extensions/LogExtensions.cs
@@ -5,6 +5,6 @@ namespace FunFair.Test.Common.Tests.Extensions;
 
 internal static partial class LogExtensions
 {
-    [LoggerMessage(EventId = 0, Level = LogLevel.Critical, Message = "Hello World. It's {now}")]
+    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Hello World. It's {now}")]
     public static partial void LogHelloWorld(this ILogger logger, DateTimeOffset now);
 }
