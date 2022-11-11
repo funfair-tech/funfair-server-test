69d098bacdfac5b6ed75f7f8f1d095f133cbdd5d
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-11 12:55:06 +0000

    Split into different methods

--- src/FunFair.Test.Common/Startup/LoggingStartup.cs
+++ src/FunFair.Test.Common/Startup/LoggingStartup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -5,12 +6,20 @@ using Serilog;
 using Serilog.Core;
 using Serilog.Enrichers.Sensitive;
 using Serilog.Events;
+using Serilog.Exceptions;
 using Xunit.Abstractions;
 
 namespace FunFair.Test.Common.Startup;
 
 internal static class LoggingStartup
 {
+    private static readonly List<IMaskingOperator> MaskingOperators = new()
+                                                                      {
+                                                                          new EmailAddressMaskingOperator(), new CreditCardMaskingOperator(), new IbanMaskingOperator()
+
+                                                                          // need to find a sane way of adding these
+                                                                      };
+
     public static IServiceCollection AddLoggingSupport(this IServiceCollection services)
     {
         return services.AddLogging(configure: AddFilters);
@@ -18,7 +27,6 @@ internal static class LoggingStartup
 
     [SuppressMessage(category: "Microsoft.Reliability", checkId: "CA2000:DisposeObjectsBeforeLosingScope", Justification = "Lives for program lifetime")]
     [SuppressMessage(category: "SmartAnalyzers.CSharpExtensions.Annotations", checkId: "CSE007:DisposeObjectsBeforeLosingScope", Justification = "Lives for program lifetime")]
-    [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global", Justification = "Not easily testable as uses third party services")]
     public static void InitializeLogging(ILoggerFactory loggerFactory, ITestOutputHelper output)
     {
         // set up Serilog logger
@@ -30,29 +38,37 @@ internal static class LoggingStartup
 
     private static Logger CreateLogger(ITestOutputHelper output)
     {
-        return new LoggerConfiguration().Enrich.FromLogContext()
-                                        .Enrich.WithSensitiveDataMasking(options =>
-                                                                         {
-                                                                             options.Mode = MaskingMode.Globally;
-                                                                             options.MaskingOperators = new()
-                                                                                                        {
-                                                                                                            new EmailAddressMaskingOperator(),
-                                                                                                            new CreditCardMaskingOperator(),
-                                                                                                            new IbanMaskingOperator()
-
-                                                                                                            // need to find a sane way of adding these
-                                                                                                        };
-                                                                             options.MaskValue = "**MASKED*";
-                                                                         })
-                                        .Enrich.WithDemystifiedStackTraces()
-                                        .Enrich.WithAssemblyName()
-                                        .Enrich.WithAssemblyVersion()
-                                        .Enrich.WithMachineName()
-                                        .Enrich.WithProcessId()
-                                        .Enrich.WithThreadId()
-                                        .WriteTo.TestOutput(testOutputHelper: output, restrictedToMinimumLevel: LogEventLevel.Debug)
-                                        .WriteTo.Debug(LogEventLevel.Debug)
+        return new LoggerConfiguration().Enrich()
+                                        .WithOutput(output)
                                         .CreateLogger();
+    }
+
+    private static LoggerConfiguration Enrich(this LoggerConfiguration configuration)
+    {
+        return configuration.Enrich.FromLogContext()
+                            .Enrich.WithExceptionDetails()
+                            .Enrich.WithSensitiveDataMasking(MaskingOptions)
+                            .Enrich.WithDemystifiedStackTraces()
+                            .Enrich.WithAssemblyName()
+                            .Enrich.WithAssemblyVersion()
+                            .Enrich.WithEnvironmentName()
+                            .Enrich.WithEnvironmentUserName()
+                            .Enrich.WithMachineName()
+                            .Enrich.WithProcessId()
+                            .Enrich.WithThreadId();
+    }
+
+    private static void MaskingOptions(SensitiveDataEnricherOptions options)
+    {
+        options.Mode = MaskingMode.Globally;
+        options.MaskingOperators = MaskingOperators;
+        options.MaskValue = "**MASKED*";
+    }
+
+    private static LoggerConfiguration WithOutput(this LoggerConfiguration configuration, ITestOutputHelper output)
+    {
+        return configuration.WriteTo.TestOutput(testOutputHelper: output, restrictedToMinimumLevel: LogEventLevel.Debug)
+                            .WriteTo.Debug(LogEventLevel.Debug);
     }
 
     private static void AddFilters(ILoggingBuilder builder)
