63195b26e434d81e024fc31fb46278408ab7076c
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-05-25 10:41:03 +0000

    [Cleanup] - Code cleanup on FunFair.Test.sln

--- src/FunFair.Test.Common/ComplexValidatorTestBase.cs
+++ src/FunFair.Test.Common/ComplexValidatorTestBase.cs
@@ -74,7 +74,8 @@ public abstract class ComplexValidatorTestBase<[DynamicallyAccessedMembers(Dynam
     [SuppressMessage(category: "ReSharper", checkId: "ParameterOnlyUsedForPreconditionCheck.Local", Justification = "Helper method")]
     protected static void AssertNamedPropertyHasErrors(ValidationResult result, string erroringProperty)
     {
-        Assert.True(result.Errors.Exists(e => e.PropertyName == erroringProperty), $"Should have had errors in {erroringProperty}, but not found found errors in {DumpPropertiesInError(result)}");
+        Assert.True(result.Errors.Exists(e => e.PropertyName == erroringProperty),
+                    $"Should have had errors in {erroringProperty}, but not found found errors in {DumpPropertiesInError(result)}");
     }
 
     protected static void AssertNamedPropertiesHaveErrors(ValidationResult result, params string[] erroringProperties)
@@ -84,9 +85,11 @@ public abstract class ComplexValidatorTestBase<[DynamicallyAccessedMembers(Dynam
         bool hasUnexpectedErrors = result.Errors.TrueForAll(error => erroringProperties.Contains(value: error.PropertyName, comparer: StringComparer.Ordinal));
         bool hasAllExpectedErrors = erroringProperties.All(error => result.Errors.Exists(p => p.PropertyName == error));
 
-        Assert.True(condition: hasUnexpectedErrors, $"Should have had errors in {DumpExpectedPropertiesInError(erroringProperties)}, but not found found errors in {DumpPropertiesInError(result)}");
+        Assert.True(condition: hasUnexpectedErrors,
+                    $"Should have had errors in {DumpExpectedPropertiesInError(erroringProperties)}, but not found found errors in {DumpPropertiesInError(result)}");
 
-        Assert.True(condition: hasAllExpectedErrors, $"Should have had errors in {DumpExpectedPropertiesInError(erroringProperties)}, but not found found errors in {DumpPropertiesInError(result)}");
+        Assert.True(condition: hasAllExpectedErrors,
+                    $"Should have had errors in {DumpExpectedPropertiesInError(erroringProperties)}, but not found found errors in {DumpPropertiesInError(result)}");
     }
 
     protected static string MakePropertyName(params string[] parts)
