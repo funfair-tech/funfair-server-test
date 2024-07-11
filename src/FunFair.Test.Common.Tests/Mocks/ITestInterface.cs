706c9b21a007d48e9009c508624bea3551366d2a
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-07-11 14:12:37 +0100

    Fixed code analysis issues

--- src/FunFair.Test.Common.Tests/Mocks/ITestInterface.cs
+++ src/FunFair.Test.Common.Tests/Mocks/ITestInterface.cs
@@ -3,6 +3,4 @@ using System.Diagnostics.CodeAnalysis;
 namespace FunFair.Test.Common.Tests.Mocks;
 
 [SuppressMessage(category: "ReSharper", checkId: "UnusedType.Global", Justification = "Used through NSubstitute DI only")]
-public interface ITestInterface
-{
-}
+public interface ITestInterface;
