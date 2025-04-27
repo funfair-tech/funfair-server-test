a59ea023923431cc66b571038bbcc658bf925b75
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 03:33:59 +0000

    Cleanup: ModelBinder.cs

--- src/FunFair.Test.Common.Tests/Mocks/Converters/Binders/ModelBinder.cs
+++ src/FunFair.Test.Common.Tests/Mocks/Converters/Binders/ModelBinder.cs
@@ -8,14 +8,8 @@ public sealed class ModelBinder : IModelBinder
 {
     public Task BindModelAsync(ModelBindingContext bindingContext)
     {
-        string modelKindName = CreatePropertyModelName(
-            prefix: null,
-            propertyName: bindingContext.ModelName
-        );
-        string? modelTypeValue = GetModelTypeValue(
-            bindingContext: bindingContext,
-            modelKindName: modelKindName
-        );
+        string modelKindName = CreatePropertyModelName(prefix: null, propertyName: bindingContext.ModelName);
+        string? modelTypeValue = GetModelTypeValue(bindingContext: bindingContext, modelKindName: modelKindName);
 
         if (modelTypeValue is null)
         {
@@ -36,10 +30,7 @@ public sealed class ModelBinder : IModelBinder
         return Task.CompletedTask;
     }
 
-    private static string? GetModelTypeValue(
-        ModelBindingContext bindingContext,
-        string modelKindName
-    )
+    private static string? GetModelTypeValue(ModelBindingContext bindingContext, string modelKindName)
     {
         return bindingContext.ValueProvider.GetValue(modelKindName).FirstValue;
     }
