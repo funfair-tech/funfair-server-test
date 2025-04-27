7ba860b1a2aba1b5f29d4235d8259bac4768e2bf
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 02:45:26 +0000

    Cleanup: ExampleRecordTypeMapper.cs

--- src/FunFair.Test.Common.Tests/ExampleRecordTypeMapper.cs
+++ src/FunFair.Test.Common.Tests/ExampleRecordTypeMapper.cs
@@ -18,11 +18,7 @@ public sealed class ExampleRecordTypeMapper : SqlMapper.TypeHandler<ExampleRecor
 
         if (StringComparer.Ordinal.Equals(x: value.Name, y: "Exception"))
         {
-            throw new ArgumentOutOfRangeException(
-                nameof(value),
-                actualValue: value.Name,
-                message: "Example"
-            );
+            throw new ArgumentOutOfRangeException(nameof(value), actualValue: value.Name, message: "Example");
         }
 
         if (StringComparer.Ordinal.Equals(x: value.Name, y: "Binary"))
