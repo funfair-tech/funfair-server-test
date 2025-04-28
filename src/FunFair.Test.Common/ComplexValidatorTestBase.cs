cdfc9ce9fc7a8dcff1b973cff4cdb66a09c49271
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-28 18:02:47 +0000

    Cleanup: ComplexValidatorTestBase.cs

--- src/FunFair.Test.Common/ComplexValidatorTestBase.cs
+++ src/FunFair.Test.Common/ComplexValidatorTestBase.cs
@@ -35,32 +35,18 @@ public abstract class ComplexValidatorTestBase<
         return result;
     }
 
-    public ValidationResult Validate(
-        TObject instance,
-        int expectedErrorCount,
-        string erroringProperty
-    )
+    public ValidationResult Validate(TObject instance, int expectedErrorCount, string erroringProperty)
     {
-        ValidationResult result = this.Validate(
-            instance: instance,
-            expectedErrorCount: expectedErrorCount
-        );
+        ValidationResult result = this.Validate(instance: instance, expectedErrorCount: expectedErrorCount);
 
         AssertOnlyNamedPropertyHasErrors(result: result, erroringProperty: erroringProperty);
 
         return result;
     }
 
-    public ValidationResult Validate(
-        TObject instance,
-        int expectedErrorCount,
-        params string[] erroringProperties
-    )
+    public ValidationResult Validate(TObject instance, int expectedErrorCount, params string[] erroringProperties)
     {
-        ValidationResult result = this.Validate(
-            instance: instance,
-            expectedErrorCount: expectedErrorCount
-        );
+        ValidationResult result = this.Validate(instance: instance, expectedErrorCount: expectedErrorCount);
 
         AssertNamedPropertiesHaveErrors(result: result, erroringProperties: erroringProperties);
 
@@ -84,15 +70,10 @@ public abstract class ComplexValidatorTestBase<
         checkId: "ParameterOnlyUsedForPreconditionCheck.Local",
         Justification = "Helper method"
     )]
-    protected static void AssertOnlyNamedPropertyHasErrors(
-        ValidationResult result,
-        string erroringProperty
-    )
+    protected static void AssertOnlyNamedPropertyHasErrors(ValidationResult result, string erroringProperty)
     {
         Assert.True(
-            result.Errors.TrueForAll(e =>
-                StringComparer.Ordinal.Equals(x: e.PropertyName, y: erroringProperty)
-            ),
+            result.Errors.TrueForAll(e => StringComparer.Ordinal.Equals(x: e.PropertyName, y: erroringProperty)),
             $"Should only have had errors in {erroringProperty}, but found errors in {string.Join(separator: ',', result.Errors.Select(selector: e => e.PropertyName).Distinct(StringComparer.Ordinal))}"
         );
     }
@@ -102,23 +83,15 @@ public abstract class ComplexValidatorTestBase<
         checkId: "ParameterOnlyUsedForPreconditionCheck.Local",
         Justification = "Helper method"
     )]
-    protected static void AssertNamedPropertyHasErrors(
-        ValidationResult result,
-        string erroringProperty
-    )
+    protected static void AssertNamedPropertyHasErrors(ValidationResult result, string erroringProperty)
     {
         Assert.True(
-            result.Errors.Exists(e =>
-                StringComparer.Ordinal.Equals(x: e.PropertyName, y: erroringProperty)
-            ),
+            result.Errors.Exists(e => StringComparer.Ordinal.Equals(x: e.PropertyName, y: erroringProperty)),
             $"Should have had errors in {erroringProperty}, but not found found errors in {DumpPropertiesInError(result)}"
         );
     }
 
-    protected static void AssertNamedPropertiesHaveErrors(
-        ValidationResult result,
-        params string[] erroringProperties
-    )
+    protected static void AssertNamedPropertiesHaveErrors(ValidationResult result, params string[] erroringProperties)
     {
         Assert.NotEmpty(erroringProperties);
 
@@ -127,10 +100,7 @@ public abstract class ComplexValidatorTestBase<
         );
         bool hasAllExpectedErrors = Array.TrueForAll(
             array: erroringProperties,
-            match: error =>
-                result.Errors.Exists(p =>
-                    StringComparer.Ordinal.Equals(x: p.PropertyName, y: error)
-                )
+            match: error => result.Errors.Exists(p => StringComparer.Ordinal.Equals(x: p.PropertyName, y: error))
         );
 
         Assert.True(
@@ -186,12 +156,8 @@ public abstract class ComplexValidatorTestBase<
         return string.Join(separator: ", ", ErroringProperties(erroringProperties));
     }
 
-    private static IOrderedEnumerable<string> ErroringProperties(
-        IReadOnlyList<string> erroringProperties
-    )
+    private static IOrderedEnumerable<string> ErroringProperties(IReadOnlyList<string> erroringProperties)
     {
-        return erroringProperties
-            .Distinct(StringComparer.Ordinal)
-            .Order(StringComparer.OrdinalIgnoreCase);
+        return erroringProperties.Distinct(StringComparer.Ordinal).Order(StringComparer.OrdinalIgnoreCase);
     }
 }
