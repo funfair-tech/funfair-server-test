e7a422917eb6bb65d34219ceb7c70cf93792d790
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-07 23:57:29 +0000

    Cleanup: LoggingFolderCleanupTestBaseTests.cs

--- src/FunFair.Test.Common.Tests/LoggingFolderCleanupTestBaseTests.cs
+++ src/FunFair.Test.Common.Tests/LoggingFolderCleanupTestBaseTests.cs
@@ -12,12 +12,14 @@ namespace FunFair.Test.Common.Tests;
 public sealed class LoggingFolderCleanupTestBaseTests : LoggingFolderCleanupTestBase
 {
     public LoggingFolderCleanupTestBaseTests(ITestOutputHelper output)
-        : base(output)
-    {
-    }
+        : base(output) { }
 
     [Fact]
-    [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0005:Avoid DateTimeOffset.UtcNow", Justification = "Unit test")]
+    [SuppressMessage(
+        category: "FunFair.CodeAnalysis",
+        checkId: "FFS0005:Avoid DateTimeOffset.UtcNow",
+        Justification = "Unit test"
+    )]
     public void OutputOutputs()
     {
         DateTimeOffset now = DateTimeOffset.UtcNow;
@@ -33,7 +35,11 @@ public sealed class LoggingFolderCleanupTestBaseTests : LoggingFolderCleanupTest
     }
 
     [Fact]
-    [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0005:Avoid DateTimeOffset.UtcNow", Justification = "Unit test")]
+    [SuppressMessage(
+        category: "FunFair.CodeAnalysis",
+        checkId: "FFS0005:Avoid DateTimeOffset.UtcNow",
+        Justification = "Unit test"
+    )]
     public void LoggingOutputs()
     {
         ILogger<LoggingTestBaseTests> logger = this.GetTypedLogger<LoggingTestBaseTests>();
@@ -53,7 +59,10 @@ public sealed class LoggingFolderCleanupTestBaseTests : LoggingFolderCleanupTest
     [Fact]
     public void MakeFaker()
     {
-        IReadOnlyList<ExampleObject> fake = MakeFake<ExampleObject>(rules: rules => rules.RuleFor(property: x => x.Name, setter: (f, _) => f.Company.Bs()), itemCount: 10);
+        IReadOnlyList<ExampleObject> fake = MakeFake<ExampleObject>(
+            rules: rules => rules.RuleFor(property: x => x.Name, setter: (f, _) => f.Company.Bs()),
+            itemCount: 10
+        );
 
         Assert.Equal(expected: 10, actual: fake.Count);
 
@@ -66,9 +75,7 @@ public sealed class LoggingFolderCleanupTestBaseTests : LoggingFolderCleanupTest
     [Fact]
     public void CanWriteToFolder()
     {
-        string filename = Path.Combine(path1: this.TempFolder,
-                                       Guid.NewGuid()
-                                           .ToString());
+        string filename = Path.Combine(path1: this.TempFolder, Guid.NewGuid().ToString());
 
         byte[] source = "Hello World"u8.ToArray();
         Assert.False(File.Exists(filename), $"File {filename} already exists before creation");
