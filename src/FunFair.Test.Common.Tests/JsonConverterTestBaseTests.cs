1b24d9322bec55c67e72d2188aaee8e00697381c
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-29 11:01:33 +0000

    Cleanup: JsonConverterTestBaseTests.cs

--- src/FunFair.Test.Common.Tests/JsonConverterTestBaseTests.cs
+++ src/FunFair.Test.Common.Tests/JsonConverterTestBaseTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace FunFair.Test.Common.Tests;
 
-[SuppressMessage(category: "ReSharper", checkId: "UnusedType.Global", Justification = "Base class for further tests")]
+
 public sealed class JsonConverterTestBaseTests : JsonConverterTestBase<ModelConverter, Model>
 {
     public JsonConverterTestBaseTests(ITestOutputHelper output)
