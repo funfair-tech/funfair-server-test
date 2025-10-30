f05cbc30de324567bfa487ce5b5aa567206609f5
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-30 23:08:53 +0000

    Cleanup: LoggingTestBaseTests.cs

--- src/FunFair.Test.Common.Tests/LoggingTestBaseTests.cs
+++ src/FunFair.Test.Common.Tests/LoggingTestBaseTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using FunFair.Test.Common.Tests.Extensions;
@@ -12,12 +12,14 @@ namespace FunFair.Test.Common.Tests;
 public sealed class LoggingTestBaseTests : LoggingTestBase
 {
     public LoggingTestBaseTests(ITestOutputHelper output)
-        : base(output)
-    {
-    }
+        : base(output) { }
 
     [Fact]
-    [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0005:Avoid DateTimeOffset.UtcNow", Justification = "Unit test")]
+    [SuppressMessage(
+        category: "FunFair.CodeAnalysis",
+        checkId: "FFS0005:Avoid DateTimeOffset.UtcNow",
+        Justification = "Unit test"
+    )]
     public void OutputOutputs()
     {
         DateTimeOffset now = DateTimeOffset.UtcNow;
@@ -33,7 +35,11 @@ public sealed class LoggingTestBaseTests : LoggingTestBase
     }
 
     [Fact]
-    [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0005:Avoid DateTimeOffset.UtcNow", Justification = "Unit test")]
+    [SuppressMessage(
+        category: "FunFair.CodeAnalysis",
+        checkId: "FFS0005:Avoid DateTimeOffset.UtcNow",
+        Justification = "Unit test"
+    )]
     public void LoggingOutputs()
     {
         ILogger<LoggingTestBaseTests> logger = this.GetTypedLogger<LoggingTestBaseTests>();
@@ -53,7 +59,10 @@ public sealed class LoggingTestBaseTests : LoggingTestBase
     [Fact]
     public void MakeFaker()
     {
-        IReadOnlyList<ExampleObject> fake = MakeFake<ExampleObject>(rules: rules => rules.RuleFor(property: x => x.Name, setter: (f, _) => f.Company.Bs()), itemCount: 10);
+        IReadOnlyList<ExampleObject> fake = MakeFake<ExampleObject>(
+            rules: rules => rules.RuleFor(property: x => x.Name, setter: (f, _) => f.Company.Bs()),
+            itemCount: 10
+        );
 
         Assert.Equal(expected: 10, actual: fake.Count);
 
