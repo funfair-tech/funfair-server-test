ba5ca914f60eaa6369af5e6c02e2af8e95ceb3ae
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-05 11:38:39 +0000

    Attempt to fix the xunit 3.2 madness

---
+++ src/FunFair.Test.Common/Logging/XUnitLoggerOptions.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FunFair.Test.Common.Logging;
+
+[DebuggerDisplay("Include Scopes: {IncludeScopes}, Cat: {IncludeCategory} LogLevel: {IncludeLogLevel}, Timestamp: {TimestampFormat}, UseUtcTimestamp: {UseUtcTimestamp}")]
+internal readonly record struct XUnitLoggerOptions(
+    bool IncludeScopes,
+    bool IncludeCategory,
+    bool IncludeLogLevel,
+    [StringSyntax(StringSyntaxAttribute.DateTimeFormat)]
+    string? TimestampFormat,
+    bool UseUtcTimestamp)
+{
+
+    public static XUnitLoggerOptions Default { get; } = new(false, false, false, null, true);
+};
