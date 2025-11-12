af832f544cb697a00a2ff0995b4990af90f7a6a9
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-12 22:03:30 +0000

    Cleanup: NamespaceError.cs

--- src/FunFair.Test.Source.Generator/Models/NamespaceError.cs
+++ src/FunFair.Test.Source.Generator/Models/NamespaceError.cs
@@ -8,19 +8,13 @@ namespace FunFair.Test.Source.Generator.Models;
 internal readonly record struct NamespaceError
 {
     public NamespaceError()
-        : this(namespaceInfo: null, errorInfo: null)
-    {
-    }
+        : this(namespaceInfo: null, errorInfo: null) { }
 
     public NamespaceError(NamespaceGeneration? namespaceInfo)
-        : this(namespaceInfo: namespaceInfo, errorInfo: null)
-    {
-    }
+        : this(namespaceInfo: namespaceInfo, errorInfo: null) { }
 
     public NamespaceError(ErrorInfo? errorInfo)
-        : this(namespaceInfo: null, errorInfo: errorInfo)
-    {
-    }
+        : this(namespaceInfo: null, errorInfo: errorInfo) { }
 
     private NamespaceError(NamespaceGeneration? namespaceInfo, ErrorInfo? errorInfo)
     {
