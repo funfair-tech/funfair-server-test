0928f63d9d66b8c1a4701df39c624aac0c5a7a3d
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-12 22:08:28 +0000

    Cleanup: XUnitLoggerOptions.cs

--- src/FunFair.Test.Common/Logging/XUnitLoggerOptions.cs
+++ src/FunFair.Test.Common/Logging/XUnitLoggerOptions.cs
@@ -3,15 +3,16 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace FunFair.Test.Common.Logging;
 
-[DebuggerDisplay("Include Scopes: {IncludeScopes}, Cat: {IncludeCategory} LogLevel: {IncludeLogLevel}, Timestamp: {TimestampFormat}, UseUtcTimestamp: {UseUtcTimestamp}")]
+[DebuggerDisplay(
+    "Include Scopes: {IncludeScopes}, Cat: {IncludeCategory} LogLevel: {IncludeLogLevel}, Timestamp: {TimestampFormat}, UseUtcTimestamp: {UseUtcTimestamp}"
+)]
 internal readonly record struct XUnitLoggerOptions(
     bool IncludeScopes,
     bool IncludeCategory,
     bool IncludeLogLevel,
-    [StringSyntax(StringSyntaxAttribute.DateTimeFormat)]
-    string? TimestampFormat,
-    bool UseUtcTimestamp)
+    [StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string? TimestampFormat,
+    bool UseUtcTimestamp
+)
 {
-
     public static XUnitLoggerOptions Default { get; } = new(false, false, false, null, true);
 };
