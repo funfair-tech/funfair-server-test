ef42c43409ffedef1fcd64e722f42fc8a53ccfe0
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-12 18:39:35 +0000

    Removed Generated

--- src/FunFair.Test.Common.Tests/Generated/FunFair.Test.Source.Generator/FunFair.Test.Source.Generator.TestAssemblyCodeGenerator/AssemblySettings.generated.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: ExcludeFromCodeCoverage]
-
-[assembly: SuppressMessage(category: "Philips.CodeAnalysis.MaintainabilityAnalyzers", checkId: "PH2140: Avoid ExcludeFromCodeCoverage", Justification = "This is a unit test assembly - no need for coverage of the test code itself")]
-[assembly: SuppressMessage(category: "Philips.CodeAnalysis.DuplicateCodeAnalyzer", checkId: "PH2071: Duplicate code", Justification = "This is a unit test assembly - Highly likely there will be duplicate code")]
-[assembly: SuppressMessage(category: "Microsoft.Performance", checkId: "CA2254: Use a fixed template in logging", Justification = "This is a unit test assembly - Although nice to have a fixed template, its not needed here")]
