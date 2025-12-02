53bd0b5c5aae5144518cef6af4f55cb89cae13e7
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-02 13:36:10 +0000

    Cleanup: TestBase.cs

--- src/FunFair.Test.Common/TestBase.cs
+++ src/FunFair.Test.Common/TestBase.cs
@@ -79,8 +79,7 @@ public abstract class TestBase
 
         const bool enable = true;
 
-        return rules(new Faker<T>().StrictMode(enable))
-            .Generate(itemCount);
+        return rules(new Faker<T>().StrictMode(enable)).Generate(itemCount);
     }
 
     protected static T GetSubstitute<T>(params object[] constructorArguments)
@@ -90,13 +89,16 @@ public abstract class TestBase
     }
 
     protected static T1 GetSubstitute<T1, T2>(params object[] constructorArguments)
-        where T1 : class where T2 : class
+        where T1 : class
+        where T2 : class
     {
         return Substitute.For<T1, T2>(constructorArguments);
     }
 
     protected static T1 GetSubstitute<T1, T2, T3>(params object[] constructorArguments)
-        where T1 : class where T2 : class where T3 : class
+        where T1 : class
+        where T2 : class
+        where T3 : class
     {
         return Substitute.For<T1, T2, T3>(constructorArguments);
     }
@@ -140,10 +142,11 @@ public abstract class TestBase
 #else
         AccumulationLogger logger = new();
 
-        ManualConfig config = ManualConfig.Create(DefaultConfig.Instance)
-                                          .AddLogger(logger)
-                                          .AddDiagnoser(new MemoryDiagnoser(new(false)))
-                                          .WithOptions(ConfigOptions.StopOnFirstError);
+        ManualConfig config = ManualConfig
+            .Create(DefaultConfig.Instance)
+            .AddLogger(logger)
+            .AddDiagnoser(new MemoryDiagnoser(new(false)))
+            .WithOptions(ConfigOptions.StopOnFirstError);
 
         Summary summary = BenchmarkRunner.Run<T>(config);
 
