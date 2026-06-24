0735d23288b1a12bf14a53e0b88bd2679dcf68a8
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-24 12:15:20 +0000

    Cleanup: TestLoggerExtension.cs

--- src/FunFair.Test.Infrastructure/Extensions/TestLoggerExtension.cs
+++ src/FunFair.Test.Infrastructure/Extensions/TestLoggerExtension.cs
@@ -12,12 +12,7 @@ public static class TestLoggerExtension
         "CA1873: Evaluation of this argument may be expensive and unnecessary if logging is disabled",
         Justification = "This is a unit test assembly - not so worried about performance"
     )]
-    public static void Received(
-        this ILogger logger,
-        LogLevel logLevel,
-        string message,
-        int received = 1
-    )
+    public static void Received(this ILogger logger, LogLevel logLevel, string message, int received = 1)
     {
         logger
             .Received(received)
