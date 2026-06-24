bf78f38c4e9f541a9b13a3ca40198d727af686fb
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-24 11:50:02 +0000

    Cleanup: RuntimeVersionInformation.cs

--- src/FunFair.Test.Source.Generator/Helpers/RuntimeVersionInformation.cs
+++ src/FunFair.Test.Source.Generator/Helpers/RuntimeVersionInformation.cs
@@ -4,8 +4,7 @@ namespace FunFair.Test.Source.Generator.Helpers;
 
 internal static class RuntimeVersionInformation
 {
-    private static readonly AssemblyName AssemblyName =
-        typeof(TestAssemblyCodeGenerator).Assembly.GetName();
+    private static readonly AssemblyName AssemblyName = typeof(TestAssemblyCodeGenerator).Assembly.GetName();
 
     public static string ToolName { get; } = AssemblyName.Name;
 
