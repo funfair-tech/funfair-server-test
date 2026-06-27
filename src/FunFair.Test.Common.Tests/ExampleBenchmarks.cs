31b5781cc8c06e1e47a67db63a9ec4f81ca2cec2
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-27 14:29:00 +0000

    Cleanup: ExampleBenchmarks.cs

--- src/FunFair.Test.Common.Tests/ExampleBenchmarks.cs
+++ src/FunFair.Test.Common.Tests/ExampleBenchmarks.cs
@@ -5,7 +5,7 @@ using BenchmarkDotNet.Attributes;
 
 namespace FunFair.Test.Common.Tests;
 
-[SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0012:Make Sealed", Justification = "Benchmarks")]
+
 public class ExampleBenchmarks
 {
     private readonly IReadOnlyList<int> _items = [.. Enumerable.Range(start: 0, count: 100)];
